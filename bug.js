d9a2d9427801c61a99e3cf5d4873e58f9373ef20
BugHunter-X <askzenai@gmail.com>
2025-01-03 22:54:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,20 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect runs only once on mount, due to the empty dependency array.
+    console.log('Component mounted!');
+    return () => {
+      console.log('Component unmounted!');
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+```
